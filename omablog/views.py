e1ba81a9db5e1636daf76f87931325ca5930a8c8
Omar Elloumi <omarelloumi842@yahoo.fr>
2025-08-25 16:41:01 +0200

    add User Signup

--- omablog/views.py
+++ omablog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from omablog.serializers import UserRegistrationSerializer
+
 
 # Create your views here.
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = UserRegistrationSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- omablog_backend_api/settings.py
+++ omablog_backend_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'omablog.apps.OmablogConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
