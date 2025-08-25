7dd2e30b42b06ade8ec1009c665bf95d9651c3a2
Omar Elloumi <omarelloumi842@yahoo.fr>
2025-08-25 16:02:39 +0200

    add CustomUser Model

--- omablog/admin.py
+++ omablog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from omablog.models import CustomUser
+
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name")
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- omablog/models.py
+++ omablog/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_img",blank=True,null=True)
+    facebook = models.URLField(max_length=255, blank=True, null=True)
+    instagram = models.URLField(max_length=255, blank=True, null=True)
+    linkedin = models.URLField(max_length=255, blank=True, null=True)
+    youtube = models.URLField(max_length=255, blank=True, null=True)
+    twitter = models.URLField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
